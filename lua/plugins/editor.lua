08c207d37e48370b47707bf58efca78c6d976e81
Josh Westbury <josh.westbury@saascg.com>
2025-10-17 16:06:17 -0500

    Add nvim-spectre for project-wide search and replace
    Added nvim-spectre plugin to provide visual, interactive search and replace across the entire project.
    
    Features:
    - Visual interface showing all matches before replacing
    - Preview of changes before applying
    - Support for regex patterns
    - File type filtering
    
    Keybindings:
    - <leader>S: Open Spectre for project-wide search/replace
    - <leader>sw: Search current word (normal) or selection (visual)
    - <leader>sp: Search/replace in current file only
    
    Requires ripgrep (already installed).

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -122,6 +122,20 @@ return {
     end,
   },
 
+  -- Project-wide search and replace with visual preview
+  {
+    'nvim-pack/nvim-spectre',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    keys = {
+      { '<leader>S', function() require('spectre').open() end, desc = 'Open Spectre (Search/Replace)' },
+      { '<leader>sw', function() require('spectre').open_visual({select_word=true}) end, desc = 'Search current word (Spectre)', mode = 'n' },
+      { '<leader>sw', function() require('spectre').open_visual() end, desc = 'Search selection (Spectre)', mode = 'v' },
+      { '<leader>sp', function() require('spectre').open_file_search({select_word=true}) end, desc = 'Search in current file (Spectre)' },
+    },
+  },
+
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
